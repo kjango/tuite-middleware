39a2d8e41986b866228360eca7b9230f441db38f
kjango@gmail.com <kjango@gmail.com@a2d48b76-e36a-aeb6-9b03-1c5b0bda4972>
2012-10-20 01:46:33 +0000

    umas telas novas...

--- TuiteClient/src/control/CtrlTuite.java
+++ TuiteClient/src/control/CtrlTuite.java
@@ -3,8 +3,11 @@ package control;
 import model.Tuite;
 
 public class CtrlTuite {
-	
-	
+	/**
+	 * 
+	 * @param t tuite that will be truncated
+	 * @return true if the tuite was truncated (over 140 chars)
+	 */
 	public boolean truncate(Tuite t){
 		if (t.getText().length() > 140){
 			t.setText(t.getText().substring(0, 139));
