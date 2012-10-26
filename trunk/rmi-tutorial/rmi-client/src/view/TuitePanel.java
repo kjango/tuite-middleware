641ff5932eec8f4d777f920651d95f00bcaa1730
kjango@gmail.com <kjango@gmail.com@a2d48b76-e36a-aeb6-9b03-1c5b0bda4972>
2012-10-26 00:09:25 +0000

    A parte da timeline da janela principal está pronta. Coloquei uns dados hardcoded pra testar.

--- trunk/rmi-tutorial/rmi-client/src/view/TuitePanel.java
+++ trunk/rmi-tutorial/rmi-client/src/view/TuitePanel.java
@@ -2,7 +2,11 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -11,19 +15,23 @@ import javax.swing.UIManager;
 import javax.swing.border.TitledBorder;
 
 import model.Tuite;
+import model.User;
+import control.CtrlClient;
 
 public class TuitePanel extends JPanel {
 	
 	private Tuite tuite;
+	private User user;
 
 	/**
 	 * Create the panel.
 	 */
 
 
-	public TuitePanel(Tuite tuite) {
+	public TuitePanel(User user, Tuite tuite) {
 		super();
 		this.tuite = tuite;
+		this.user = user;
 		setSize(new Dimension(450, 80));
 		setPreferredSize(new Dimension(450, 80));
 		setMaximumSize(new Dimension(450, 80));
@@ -34,8 +42,13 @@ public class TuitePanel extends JPanel {
 		setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), tuite.getMyUser().getRealName() + " @ " + tuite.getCreatedAt(), TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		setLayout(new BorderLayout(0, 0));
 		
-		JButton btnFollowUnfollow = new JButton("Follow / Unfollow");
-		btnFollowUnfollow.setPreferredSize(new Dimension(80, 13));
+		CtrlClient cc = new CtrlClient();
+		String btnText = "Follow";
+		if(cc.follows(user, tuite.getMyUser())){
+			btnText = "Unfollow";
+		}
+		JButton btnFollowUnfollow = new JButton(btnText);
+		btnFollowUnfollow.setPreferredSize(new Dimension(90, 13));
 		add(btnFollowUnfollow, BorderLayout.EAST);
 		
 		JLabel labelTuite = new JLabel(tuite.getText());
@@ -44,7 +57,18 @@ public class TuitePanel extends JPanel {
 		JLabel lblImage = new JLabel("");
 		add(lblImage, BorderLayout.WEST);
 		
-		lblImage.setIcon(new ImageIcon(tuite.getMyUser().getPhoto()));
+		BufferedImage photo = null;
+		photo = tuite.getMyUser().getPhoto();
+		if (photo == null){
+			try {
+				photo = ImageIO.read(new File("noImg.jpg"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+//		lblImage.setIcon(new ImageIcon(tuite.getMyUser().getPhoto()));
+		lblImage.setIcon(new ImageIcon(photo));
 	}
 
 }
